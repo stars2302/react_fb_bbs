a924160b301bfe729c0d35ff7018f07a33222eab
stars2302 <stars2302@naver.com>
2023-10-12 11:43:21 +0900

    [FEAT] 컬렉션, 문서추가 기능

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -30,3 +34,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 export const authService = getAuth(app);
+export const db = getFirestore(app);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+//https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ko&authuser=0
+// firebase 값 추가 참조사이트
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { db } from "../firebase";
 
 
-const Home = ()=> <h2>Home Page</h2>
+const Home = ()=> {
+  const [post,setPost] = useState('');
+
+  const onChange = (e)=>{
+    // const value = e.target.value; //ECMA script 2012인가... 무튼 ES6이전 문법임
+    const {target:{value}} = e;
+    setPost(value);
+  }
+
+
+  //await은 async(비동기)방식이라 async를 앞에 붙여야 됨 
+  const onSubmit = async (e)=>{
+    e.preventDefault();
+
+    //await setDoc(doc(db, "cities", "new-city-id"), data);
+    //await setDoc(doc(firebase.js에서 추출한, "컬렉션이름","문서이름"),넣을 데이터);
+    const docRef = await addDoc(collection(db, "posts"), {
+      content: post,
+      date: serverTimestamp() //오늘 날짜 시분초 - firebase지원 firebase최고!
+    });
+    console.log("Document written with ID: ", docRef.id);
+  }
+  
+  return(
+    <div>
+      <form onSubmit={onSubmit}>
+        <input type="text" name="" placeholder="포스트 쓰기" onChange={onChange} value={post}/>
+        <button>입력</button>
+      </form>
+    </div>
+  )
+} //comp Home
 
 
 
