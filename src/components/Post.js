16a76e3165d70fb1439c91fa4c84edf3d3741d05
stars2302 <stars2302@naver.com>
2023-10-13 10:00:39 +0900

    [FEAT] 글 삭제 기능 추가

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,17 +1,34 @@
 import React from "react";
+import { doc, deleteDoc } from "firebase/firestore";
+import { db } from "../firebase";
 
-const Post = ({postObj,userConfirm})=>(
-  <li>
-    <h4>{postObj.content}</h4>
-    { userConfirm && (
-      <>
-    <button>Delete</button>
-    <button>Edit</button>
-    </>
-    )
-    }
-  </li>
 
+
+//await deleteDoc(doc(db, '컬렉션','문서이름')); 문서삭제
+//https://firebase.google.com/docs/firestore/manage-data/delete-data?hl=ko&authuser=0
+
+
+const Post = ({postObj,userConfirm})=>{
+  console.log(postObj);
+  const deletePost = async ()=>{
+    if(window.confirm('정말로 삭제하시겠습니까?')){
+      await deleteDoc(doc(db,'posts',postObj.id));
+    } 
+  }
+  
+  
+  
+  return (
+    <li>
+      <h4>{postObj.content}</h4>
+      { userConfirm && (
+        <>
+        <button onClick={deletePost}>Delete</button>
+        <button>Edit</button>
+        </>
+      )}
+    </li>
   )
+}
 
 export default Post;
