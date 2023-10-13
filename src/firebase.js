28e52349a00e4a92ec196edd184ecd6e493c93e3
stars2302 <stars2302@naver.com>
2023-10-13 12:50:30 +0900

    [FEAT] 이미지 첨부 기능 추가

--- src/firebase.js
+++ src/firebase.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 
 
@@ -35,3 +36,4 @@ const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const authService = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,13 +1,17 @@
 import React, { useEffect, useState } from "react";
 //https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ko&authuser=0
 // firebase 값 추가 참조사이트
-import { collection, getDocs, addDoc, serverTimestamp, query, onSnapshot, orderBy } from "firebase/firestore";
+import { collection, addDoc, serverTimestamp, query, onSnapshot, orderBy } from "firebase/firestore";
 import { db } from "../firebase";
 import Post from "../components/Post";
+import { getStorage, ref,uploadString ,getDownloadURL } from "firebase/storage";
+import { v4 as uuidv4 } from 'uuid';
 
 const Home = ({userObj})=> {
   const [post,setPost] = useState('');
   const [posts,setPosts] = useState([]);
+  const [attachment,setAttachment] = useState(); //이미지 //기본값 undefined
+  // const [inputFile, setInputFile] = useState();
   
 
   const onChange = (e)=>{
@@ -21,6 +25,28 @@ const Home = ({userObj})=> {
   const onSubmit = async (e)=>{
     e.preventDefault();
 
+    //참조만들기 및 파일업로드
+    const storage = getStorage();
+    const storageRef = ref(storage, `${userObj}/${uuidv4()}`);
+
+    uploadString(storageRef, attachment, 'data_url').then(async (snapshot) => {
+      const attachmentUrl = await getDownloadURL(storageRef);
+
+      try{
+        //await setDoc(doc(db, "cities", "new-city-id"), data);
+        //await setDoc(doc(firebase.js에서 추출한, "컬렉션이름","문서이름"),넣을 데이터);
+        await addDoc(collection(db, "posts"), {
+          content: post,
+          date: serverTimestamp(), //오늘 날짜 시분초 - firebase지원 firebase최고!
+          uid: userObj,
+          attachmentUrl
+        });
+      } catch(error){
+        console.log(error);
+      }
+    });
+
+
     /*
     //에러확인하기!
     try{
@@ -30,18 +56,7 @@ const Home = ({userObj})=> {
     }
     */
 
-    try{
-      //await setDoc(doc(db, "cities", "new-city-id"), data);
-      //await setDoc(doc(firebase.js에서 추출한, "컬렉션이름","문서이름"),넣을 데이터);
-      const docRef = await addDoc(collection(db, "posts"), {
-        content: post,
-        date: serverTimestamp(), //오늘 날짜 시분초 - firebase지원 firebase최고!
-        uid: userObj
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch(error){
-      console.log(error);
-    }
+    
   }
 
   /*
@@ -87,10 +102,47 @@ const Home = ({userObj})=> {
   },[]);
   console.log(posts);
   
+  const conFileChange = (e)=>{
+    console.log(e.target.files[0]);//첨부파일정보
+    const {target:{files}} = e;
+    const theFile = files[0];
+    const reader = new FileReader(); //filefeader를 사용할 수 있도록 객체 생성
+
+    //onloadend - 로드가 끝나면~
+    reader.onloadend = (e)=>{
+      setAttachment(e.target.result);
+    }
+    reader.readAsDataURL(theFile); //엄청 긴... 문자열로 절대경로..? 만들어서 반환해주는듯..
+  }
+
+  const onFileClear = ()=>{
+    setAttachment(null);
+    // document.querySelector('#attachment').value = null;
+  }
+
+
+
+
+
+  //input의 attribute accept는 파일형식을 지정할 수 있음!!
+  //https://developer.mozilla.org/ko/docs/Web/API/FileReader/readAsDataURL 파일 미리보기 filereader mdn
   return(
     <div>
       <form onSubmit={onSubmit}>
-        <input type="text" name="" placeholder="포스트 쓰기" onChange={onChange} value={post}/>
+        <p>
+          <label htmlFor="content">내용: </label>
+          <input type="text" name="" placeholder="포스트 쓰기" onChange={onChange} value={post}/>
+        </p>
+        <p>
+          <label htmlFor="attachment">첨부이미지: </label>
+          <input type="file" accept="images/*" onChange={conFileChange} />
+          {attachment && 
+            <>
+              <img src={attachment} alt="" width="250"/>
+              <button type="button" onClick={onFileClear}>이미지취소</button>
+            </>
+          }
+        </p>
         <button>입력</button>
       </form>
       <ul>
